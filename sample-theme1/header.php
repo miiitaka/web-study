9cf2a91112fd35a85a5b0f071853d017dae4fabd
田崎貴大 <qxgxyea_h@yahoo.co.jp>
2018-05-29 19:47:16 +0900

    2018.05.24 commit2.

---
+++ sample-theme1/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>ZOO LOGICAL</title>
+<link rel="stylesheet" href="css/common.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/responsive.css" media="screen and (max-width: 480px)">
+</head>
+<body>
+  <header>
+    <h1>
+      <a href="index.html">
+        <img src="images/logo.png" width="244" height="76" alt="ZOO LOGICAL">
+      </a>
+    </h1>
+  </header>
+
+  <nav class="nav-global">
+    <ul>
+      <li><a href="about.html">動物園について</a></li>
+      <li><a href="guide.html">ガイドのご案内</a></li>
+      <li><a href="animals.html">人気の動物たち</a></li>
+      <li><a href="contact.html">お問い合わせ</a></li>
+    </ul>
+  </nav>
