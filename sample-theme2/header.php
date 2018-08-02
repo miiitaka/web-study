efefb186fa0d61011926a33a1f455c95365e0368
田崎貴大 <qxgxyea_h@yahoo.co.jp>
2018-08-02 21:12:05 +0900

    2018.08.02 commit.

--- sample-theme2/header.php
+++ sample-theme2/header.php
@@ -9,7 +9,7 @@
 <body <?php body_class(); ?>>
 	<header>
 		<h1>
-			<a href="<?php home_url(); ?>">
+			<a href="<?php echo home_url(); ?>">
 				<?php
 					$custom_logo_id = get_theme_mod( 'custom_logo' );
 					$image = wp_get_attachment_image_src( $custom_logo_id, 'full');
