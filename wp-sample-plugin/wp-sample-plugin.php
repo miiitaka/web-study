bee4be4cfe5260c1cc0c0efb089f0d658e317a49
田崎貴大 <qxgxyea_h@yahoo.co.jp>
2018-10-16 21:02:32 +0900

    2018.10.16 commit.

---
+++ wp-sample-plugin/wp-sample-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WordPress Sample Plugin
+Plugin URI: https://github.com/miiitaka/web-study/tree/201801/wp-sample-plugin
+Descrition: WordPress Plugin sample build.
+Version: 1.0.0
+Author: web-study
+Author URI: https://github.com/miiitaka/web-study/tree/201801/wp-sample-plugin
+License: GPLv2 or later
+*/
