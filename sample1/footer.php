6731befead402ece494010eaaa780e6e93ed41c0
田崎貴大 <qxgxyea_h@yahoo.co.jp>
2018-11-29 17:06:00 +0900

    2018.11.28 commit.

---
+++ sample1/footer.php
@@ -0,0 +1,13 @@
+  <footer>
+    <p class="pagetop"><a id="pageTop" href="javascript:void(0);">ページトップへ戻る</a></p>
+    <address>
+      山梨県富士吉田市X-X-X
+      電話 0120-XXX-XXX
+      営業時間 9:00〜17:00(休園日：月曜日、年末年始)
+    </address>
+    <p class="copyright">
+      <small>Copyright 2014 ZOO LOGICAL All rights reserved.</small>
+    </p>
+  </footer>
+</body>
+</html>
---
+++ sample1/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>ZOO LOGICAL</title>
+<link rel="stylesheet" href="css/common.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/responsive.css" media="screen and (max-width:480px)">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="js/slide.js"></script>
+</head>
