e47982575a0d71a0de25c77a36400e9936bc7d7f
田崎貴大 <qxgxyea_h@yahoo.co.jp>
2019-02-25 21:04:54 +0900

    2019.02.25 commit.

--- game-trump-lesson/concentration/main.js
+++ game-trump-lesson/concentration/main.js
@@ -1,11 +1,11 @@
 $(function() {
 	//とりあえず全部のトランプの背面を表示
-	for(var i = 0; i < 4; i++) {
-		for(var j = 0; j < 13; j++) {
-			var li = '<li class="card is-surface"><img src="../images/z01.png" alt=""></li>';
-			$(li).appendTo('#table');
-		}
-	}
+	// for(var i = 0; i < 4; i++) {
+	// 	for(var j = 0; j < 13; j++) {
+	// 		var li = '<li class="card is-surface"><img src="../images/z01.png" alt=""></li>';
+	// 		$(li).appendTo('#table');
+	// 	}
+	// }
 
 	//とりあえず表を全部表示
 	// var kind = ['h', 'd', 'c', 's'];
@@ -18,11 +18,19 @@ $(function() {
 
 	//jQueryらしく書く
 	var
-		$table = $('#table'),
-		$li    = $('<li>'),
-		$img   = $('<img>'),
-		kind   = ['h', 'd', 'c', 's'],
-		cards  = [];
+		$table       = $('#table'),
+		$li          = $('<li>'),
+		$img         = $('<img>'),
+		kind         = ['h', 'd', 'c', 's'],
+		cards        = [],
+		select_index = [],
+		select_num   = '',
+		$timer       = $('#timer'),
+		default_time = 180,
+		time         = default_time;
+
+	$timer.text(setTime());
+
 	for(var i = 0; i < 4; i++) {
 		for(var j = 1; j <= 13; j++) {
 			// $li
@@ -45,13 +53,46 @@ $(function() {
 		$li
 			.clone()
 			.addClass('card is-surface')
+			.data('num', cards[i].replace(/[^0-9]/g, ''))
 			.append(
 				$img
 					.clone()
 					.attr('src', '../images/' + cards[i])
+					.addClass('card_reverse')
+			)
+			.append(
+				$img
+					.clone()
+					.attr('src', '../images/z01.png')
+					.addClass('card_surface')
 			)
 			.appendTo($table);
 	}
+
+	$table.on('click', 'li', function() {
+		if(select_index.length >= 2) {
+			return false;
+		}
+		$(this).toggleClass('is-surface').toggleClass('is-reverse');
+
+		if(select_num === '') {
+			//１枚目をめくったときの処理
+			select_num = $(this).data('num');
+			select_index.push($(this).index());
+		} else {
+			//２枚目をめくったときの処理
+			if($(this).index() !== select_index[0]) {
+				select_index.push($(this).index());
+				if($(this).data('num') === select_num) {
+					//あたりのとき 
+					setTimeout(card_ok, 1000);
+				} else {
+					//はずれのとき
+					setTimeout(card_reverse, 1000);
+				}
+			}
+		}
+	});
 
 	// シャッフル機能
 	function shuffle() {
@@ -72,4 +113,37 @@ $(function() {
 			cards[j]     = tmp;
 		}
 	}
+
+	//外れたときの処理
+	function card_reverse() {
+		$table.find('li').eq(select_index[0]).toggleClass('is-surface').toggleClass('is-reverse');
+		$table.find('li').eq(select_index[1]).toggleClass('is-surface').toggleClass('is-reverse');
+		select_num   = '';
+		select_index = [];
+	}
+
+	//あたったときの処理
+	function card_ok() {
+		$table.find('li').eq(select_index[0]).addClass('hit');
+		$table.find('li').eq(select_index[1]).addClass('hit');
+		select_num   = '';
+		select_index = [];
+	}
+	
+	//タイマーをセット
+	function setTime() {
+		minute = ('0' + Math.floor(time / 60)).slice(-2);
+		second = ('0' + time % 60).slice(-2);
+		return minute + '：' + second;
+	}
 });
+
+
+
+
+
+
+
+
+
+
