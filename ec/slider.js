1b95823716480341b4d250c994678e9edddf4003
田崎貴大 <qxgxyea_h@yahoo.co.jp>
2019-02-27 21:09:38 +0900

    2019.02.27 commit.

--- ec/slider.js
+++ ec/slider.js
@@ -6,4 +6,18 @@ $(function() {
 		"autoControls": true,
 		"captions": true
 	});
+
+	$("#checked_items").bxSlider({
+		'minSlides': 1,
+		'maxSlides': 9,
+		'prevText': '<',
+		'nextText': '>',
+		'slideMargin': 17,
+		'slideWidth': 96
+	});
+
+	$("#product_image").find("li").on("click", function() {
+		var imgPath = $(this).find("img").attr("src");
+		$("#main_image").attr("src",imgPath);
+	});
 });
