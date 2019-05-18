7caeed3538a11bea42ef96abff57fb8102ae85bf
garage <garage@garagenoiMac.local>
2019-05-18 14:31:12 +0900

    2019.05.18 commit.

--- my-app/src/Hello.js
+++ my-app/src/Hello.js
@@ -3,10 +3,32 @@ import React, { Component } from 'react';
 class Hello extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      power: false,
+      date: {
+        year: '2019',
+        month: '05',
+        day: '18'
+      }
+    };
+    this.submitHandler = this.submitHandler.bind(this);
   };
+  submitHandler(e) {
+    e.preventDefault();
+    console.log(this.state);
+    this.setState({
+      power: true
+    });
+  }
   render() {
     return (
-      <div>こんにちは{this.props.name}さん</div>
+      <div>
+        <p>こんにちは{this.props.name}さん</p>
+        <form onSubmit={this.submitHandler}>
+          <p>{this.state.power ? "ON" : "OFF"}</p>
+          <button type="submit">Switch</button>
+        </form>
+      </div>
     );
   }
 }
